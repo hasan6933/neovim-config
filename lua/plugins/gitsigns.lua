a98723fff3276636b44e4bb06856a924dcdfd43a
Mohammad Hasan <hasan8218316933@gmail.com>
2025-10-14 11:56:31 +0530

    changed current_line_blame delay to 100 and use_focus to false

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,15 @@ return {
 	config = function()
 		require("gitsigns").setup({
 			current_line_blame = true,
+			attach_to_untracked = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 100,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = false,
+			},
 		})
 	end,
 }
