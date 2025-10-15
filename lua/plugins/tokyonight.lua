9594afd0ec0746276deaab589206d0843be2a0a7
Mohammad Hasan <hasan8218316933@gmail.com>
2025-10-15 10:55:08 +0530

    changed color.green to #d2ffa1

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -28,6 +28,7 @@ return {
 				colors.warning = colors.yellow
 				colors.info = colors.blue
 				colors.hint = colors.green1
+				colors.green = "#D2FFA1"
 			end,
 			on_highlights = function(highlights, colors)
 				highlights.NvimTreeWinSeparator = {
