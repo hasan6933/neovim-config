1163b214114e9bf9507b7814d19c3adae0c2f935
Mohammad Hasan <hasan8218316933@gmail.com>
2025-10-16 21:57:01 +0530

    updated all plugins

--- lua/plugins/rainbow-delimeter.lua
+++ lua/plugins/rainbow-delimeter.lua
@@ -1,16 +1,16 @@
 return {
-	"HiPhish/rainbow-delimiters.nvim",
-	config = function()
-		require("rainbow-delimiters.setup").setup({
-			highlight = {
-				"RainbowDelimiterViolet",
-				"RainbowDelimiterRed",
-				"RainbowDelimiterYellow",
-				"RainbowDelimiterBlue",
-				"RainbowDelimiterOrange",
-				"RainbowDelimiterGreen",
-				"RainbowDelimiterCyan",
-			},
-		})
-	end,
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+        require("rainbow-delimiters.setup").setup({
+            highlight = {
+                "RainbowDelimiterViolet",
+                "RainbowDelimiterRed",
+                "RainbowDelimiterYellow",
+                "RainbowDelimiterBlue",
+                "RainbowDelimiterOrange",
+                "RainbowDelimiterGreen",
+                "RainbowDelimiterCyan",
+            },
+        })
+    end,
 }
--- lua/plugins/trouble-nvim.lua
+++ lua/plugins/trouble-nvim.lua
@@ -1,73 +1,73 @@
 return {
-	"folke/trouble.nvim",
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
-	cmd = "Trouble",
-	keys = {
-		{
-			"<leader>xx",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xX",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>cs",
-			"<cmd>Trouble symbols toggle focus=false<cr>",
-			desc = "Symbols (Trouble)",
-		},
-		{
-			"<leader>cl",
-			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-			desc = "LSP Definitions / references / ... (Trouble)",
-		},
-		{
-			"<leader>xL",
-			"<cmd>Trouble loclist toggle<cr>",
-			desc = "Location List (Trouble)",
-		},
-		{
-			"<leader>xQ",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
-		},
-	},
-	config = function()
-		require("trouble").setup({
-			icons = {
-				folder_closed = "",
-				folder_open = "",
-				kinds = {
-					Array = " ",
-					Boolean = " ",
-					Class = " ",
-					Constant = " ",
-					Constructor = " ",
-					Enum = " ",
-					EnumMember = " ",
-					Event = " ",
-					Field = " ",
-					File = " ",
-					Function = "󰊕 ",
-					Interface = " ",
-					Key = " ",
-					Method = " ",
-					Module = " ",
-					Namespace = " ",
-					Null = " ",
-					Number = " ",
-					Object = " ",
-					Operator = " ",
-					Package = " ",
-					Property = " ",
-					String = " ",
-					Struct = " ",
-					TypeParameter = " ",
-					Variable = " ",
-				},
-			},
-		})
-	end,
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
+    config = function()
+        require("trouble").setup({
+            icons = {
+                folder_closed = "",
+                folder_open = "",
+                kinds = {
+                    Array = " ",
+                    Boolean = " ",
+                    Class = " ",
+                    Constant = " ",
+                    Constructor = " ",
+                    Enum = " ",
+                    EnumMember = " ",
+                    Event = " ",
+                    Field = " ",
+                    File = " ",
+                    Function = "󰊕 ",
+                    Interface = " ",
+                    Key = " ",
+                    Method = " ",
+                    Module = " ",
+                    Namespace = " ",
+                    Null = " ",
+                    Number = " ",
+                    Object = " ",
+                    Operator = " ",
+                    Package = " ",
+                    Property = " ",
+                    String = " ",
+                    Struct = " ",
+                    TypeParameter = " ",
+                    Variable = " ",
+                },
+            },
+        })
+    end,
 }
