9ec204338d2cf0521cabe77f4a36f661ff9aaba1
Mohammad Hasan <hasan8218316933@gmail.com>
2025-10-17 15:54:09 +0530

    add lazy.nvim in lazydev

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -5,6 +5,7 @@ return {
 		opts = {
 			library = {
 				"snacks.nvim",
+				"lazy",
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
