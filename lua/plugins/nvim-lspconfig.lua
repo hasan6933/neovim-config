36481bdf0ee49f6aa96a8a23f92c594546fd6a3d
Mohammad Hasan <hasan8218316933@gmail.com>
2025-10-17 07:12:47 +0530

    update emmylua_ls config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,104 +1,116 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = { "saghen/blink.cmp" },
-    config = function()
-        vim.lsp.config("emmylua_ls", {
-            cmd = { 'emmylua_ls' },
-            filetypes = { 'lua' },
-            root_markers = {
-                '.luarc.json',
-                '.emmyrc.json',
-                '.luacheckrc',
-                '.git',
-            },
-            workspace_required = false,
-        })
-        vim.lsp.config("rust_analyzer", {
-            capabilities = {
-                experimental = {
-                    commands = {
-                        commands = {
-                            "rust-analyzer.showReferences",
-                            "rust-analyzer.runSingle",
-                            "rust-analyzer.debugSingle",
-                        },
-                    },
-                },
-            },
-            settings = {
-                ["rust-analyzer"] = {
-                    completion = {
-                        privateEditable = {
-                            enable = true,
-                        },
-                        fullFunctionSignatures = {
-                            enable = true,
-                        },
-                    },
-                    inlayHints = {
-                        expressionAdjustmentHints = {
-                            enable = "always",
-                        },
-                        discriminantHints = {
-                            enable = "always",
-                        },
-                        genericParameterHints = {
-                            type = {
-                                enable = true,
-                            },
-                            lifetime = {
-                                enable = true,
-                            },
-                        },
-                        implicitDrops = {
-                            enable = true,
-                        },
-                        implicitSizedBoundHints = {
-                            enable = true,
-                        },
-                        lifetimeElisionHints = {
-                            useParameterNames = true,
-                            enable = true,
-                        },
-                    },
-                    semanticHighlighting = {
-                        operator = {
-                            specialization = {
-                                enable = true,
-                            },
-                        },
-                        punctuation = {
-                            enable = true,
-                            separate = {
-                                macro = {
-                                    bang = true,
-                                },
-                            },
-                            specialization = {
-                                enable = true,
-                            },
-                        },
-                    },
-                },
-            },
-        })
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
+	config = function()
+		vim.lsp.config("emmylua_ls", {
+			cmd = { "emmylua_ls" },
+			filetypes = { "lua" },
+			root_markers = {
+				".luarc.json",
+				".emmyrc.json",
+				".luacheckrc",
+				".git",
+			},
+			workspace_required = true,
+			settings = {
+				Lua = {
+					diagnostics = {
+						diagnosticInterval = 0,
+						globals = { "*" },
+						globalsRegex = { "*" },
+					},
+					runtime = {
+						version = "LuaLatest",
+					},
+				},
+			},
+		})
+		vim.lsp.config("rust_analyzer", {
+			capabilities = {
+				experimental = {
+					commands = {
+						commands = {
+							"rust-analyzer.showReferences",
+							"rust-analyzer.runSingle",
+							"rust-analyzer.debugSingle",
+						},
+					},
+				},
+			},
+			settings = {
+				["rust-analyzer"] = {
+					completion = {
+						privateEditable = {
+							enable = true,
+						},
+						fullFunctionSignatures = {
+							enable = true,
+						},
+					},
+					inlayHints = {
+						expressionAdjustmentHints = {
+							enable = "always",
+						},
+						discriminantHints = {
+							enable = "always",
+						},
+						genericParameterHints = {
+							type = {
+								enable = true,
+							},
+							lifetime = {
+								enable = true,
+							},
+						},
+						implicitDrops = {
+							enable = true,
+						},
+						implicitSizedBoundHints = {
+							enable = true,
+						},
+						lifetimeElisionHints = {
+							useParameterNames = true,
+							enable = true,
+						},
+					},
+					semanticHighlighting = {
+						operator = {
+							specialization = {
+								enable = true,
+							},
+						},
+						punctuation = {
+							enable = true,
+							separate = {
+								macro = {
+									bang = true,
+								},
+							},
+							specialization = {
+								enable = true,
+							},
+						},
+					},
+				},
+			},
+		})
 
-        vim.lsp.config("ruff", {
-            init_options = {
-                settings = {
-                    -- Ruff language server settings go here
-                    lint = {
-                        enable = false,
-                    },
-                },
-            },
-        })
+		vim.lsp.config("ruff", {
+			init_options = {
+				settings = {
+					-- Ruff language server settings go here
+					lint = {
+						enable = false,
+					},
+				},
+			},
+		})
 
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            pattern = "*.rs",
-            callback = function(args)
-                vim.lsp.codelens.refresh({ bufnr = args.buf })
-            end,
-        })
-    end,
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			pattern = "*.rs",
+			callback = function(args)
+				vim.lsp.codelens.refresh({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
