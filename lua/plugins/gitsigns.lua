e0d51602955c2a0e555bfa537604800540fc1c98
Mohammad Hasan <hasan8218316933@gmail.com>
2025-10-26 16:24:56 +0530

    add config for gitsigns.lua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,18 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup({})
+		require("gitsigns").setup({
+			attach_to_untracked = true,
+			current_line_blame = true,
+			numhl = false,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 100,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+		})
 	end,
 }
