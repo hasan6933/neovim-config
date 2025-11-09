8e88648c8176aa94622a90d744a8e3d061ae6744
Mohammad Hasan <hasan8218316933@gmail.com>
2025-11-09 13:40:54 +0530

    update

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,6 +8,10 @@ return {
 					workspace = {
 						library = vim.api.nvim_get_runtime_file("", true),
 					},
+					diagnostics = {
+						disable = { "param-type-mismatch" },
+						globalRegex = { "" },
+					},
 				},
 			},
 		})
