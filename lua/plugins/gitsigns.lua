31b7c45c495d0dda022950ebebebc8d589c6e5f1
Mohammad Hasan <hasan8218316933@gmail.com>
2025-11-10 16:19:55 +0530

    update

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -16,6 +16,8 @@ return {
 			signs = {
 				add = { text = "⎥" },
 				change = { text = "⎥" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 				untracked = { text = "⎥" },
 			},
